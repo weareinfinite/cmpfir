d6a9a54ec2089f30c77528bf676f769b3ea11c6a
Sarath <sarath.hacks@gmail.com>
2018-09-20 11:12:08 +0530

    :apple: Dashboard component

---
+++ src/components/project/ProjectList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProjectSummery from './ProjectSummery'
+
+export default () => {
+  return (
+    <div className='project-list section'>
+      <ProjectSummery />
+      <ProjectSummery />
+      <ProjectSummery />
+    </div>
+  )
+}
---
+++ src/components/project/ProjectSummery.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div className='card z-depth-0 project-summery'>
+      <div className='card-content grey-text text-darken-3'>
+        <span className='card-title'>Project Title</span>
+        <p>Posted By : Sarath</p>
+        <p className='grey-text'>20 Sep</p>
+      </div>
+    </div>
+  )
+}
